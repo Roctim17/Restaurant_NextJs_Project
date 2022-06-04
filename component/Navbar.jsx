104dca5beb8464bdb8b385d7dc7dd659ca4035ad
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-06-05 01:23:15 +0600

    add Navbar

--- component/Navbar.jsx
+++ component/Navbar.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import style from '../styles/Navbar.module.css'
+import Image from 'next/image'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className={style.container}>
+            <div className={style.item}>
+                <div className={style.callBtn}>
+                    <Image src="/image/images/icons/phone-icon--25.png" alt="" width={32} height={32} />
+                </div>
+                <div className={style.texts}>
+                    <div className={style.text}>ORDER NOW!</div>
+                    <div className={style.text}>013 000 000</div>
+
+                </div>
+            </div>
+            <div className={style.item}>
+                <ul className={style.list}>
+                    <li className={style.listitem}></li>
+
+                </ul>
+            </div>
+            <div className={style.item}>right </div>
+        </div>
     )
 }
 
