6252f77ba8e67af027727ea202ac5e524f87db52
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-06-05 02:35:35 +0600

    add slider image

--- component/Slider.jsx
+++ component/Slider.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
-import style from '../styles/slider.module.css'
+import style from '../styles/Slider.module.css'
 import Image from 'next/image'
 
 const Slider = () => {
+    const images = [
+        "/image/images/breakfast/breakfast1.png",
+        "/image/images/dinner/dinner1.png",
+        "/image/images/lunch/lunch1.png",
+    ];
     return (
         <div className={style.container}>
-            <Image src="/public/img/arrowl.png" alt="" layout='fill'></Image>
-            <div className={style.wrapper}>
-                <div className={style.imgcontainer}></div>
-                <Image src="/public/img/pizza.png" alt="" layout='fill' />
-
+            <div className={style.arrowContainer} style={{ left: 0 }} >
+                <Image src="/img/arrowl.png" alt="" layout="fill" objectFit="contain" />
             </div>
-            <Image src="/public/img/arrowr.png" alt="" layout='fill'></Image>
+            <div className={style.wrapper} >
+                {images.map((img, i) => (
+                    <div className={style.imgContainer} key={i}>
+                        <Image src={img} alt="" layout="fill" objectFit="contain" />
+                    </div>
+                ))}
+            </div>
+            <div className={style.arrowContainer} style={{ right: 0 }} >
+                <Image src="/img/arrowr.png" layout="fill" alt="" objectFit="contain" />
+            </div>
         </div>
     )
 }
